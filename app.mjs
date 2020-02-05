bc662400a4d93aa69ce110650c6a8faab346ec6b
Yurii Faievskyi <urfaevskii@gmail.com>
2020-02-05 18:15:12 +0200

    feat: add ES to express app

---
+++ app.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index.mjs';
+import usersRouter from './routes/users.mjs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
